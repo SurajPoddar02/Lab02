23c7fe19414b405702edbd418ebdfd501347241c
SurajPoddar02 <96971702+SurajPoddar02@users.noreply.github.com>
2023-01-19 01:44:01 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello world!, this is RITIK PATIDAR");
+        System.out.println("Hello world!, this is Suraj Poddar");
     }
 }
